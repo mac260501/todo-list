cfe95469b9b25e2ffe0ba05be0e9f19f427b75f6
mohammad <mac.26.05.01@gmail.com>
2023-02-13 20:29:44 -0500

    Added basic task functionality. Next up: refactor code - add a task class and a UI module.

--- src/inbox.js
+++ src/inbox.js
@@ -1,13 +1,76 @@
+const addTask = document.createElement("div");
+addTask.innerHTML = "<b>+</b> &nbsp; Add Task";
+addTask.classList.add("task");
+addTask.id = "addTask";
+
 function buildInboxSection(taskContainer) {
   const heading2 = document.createElement("h2");
   heading2.textContent = "Inbox";
 
-  const addTask = document.createElement("div");
-  addTask.innerHTML = "<b>+</b> &nbsp;&nbsp; Add Task";
-  addTask.classList.add("add-task");
+  addTask.addEventListener("click", (event) => {
+    taskContainer.removeChild(addTask);
+    let { newTask, input } = createTask(taskContainer);
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      const input = document.getElementById("task");
+      if (input !== null && input.value.length !== 0) {
+        const newTask = input.parentElement;
+        const text = newTask.querySelector(".task-text");
+        text.textContent = input.value;
+        text.style.display = "block";
+        text.style.flex = "1";
+        newTask.removeChild(input);
+        taskContainer.appendChild(addTask);
+      }
+    }
+  });
 
   taskContainer.appendChild(heading2);
   taskContainer.appendChild(addTask);
+}
+
+function createTask(taskContainer) {
+  const newTask = document.createElement("div");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("checkbox");
+  checkbox.value = "done";
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.id = "task";
+  input.name = "task";
+  input.maxLength = "50";
+  input.autofocus = true;
+
+  const text = document.createElement("div");
+  text.classList.add("task-text");
+  text.style.display = "none";
+
+  const closeIcon = document.createElement("div");
+  closeIcon.textContent = "x";
+  closeIcon.classList.add("close-icon");
+
+  newTask.appendChild(checkbox);
+  newTask.appendChild(input);
+  newTask.appendChild(text);
+  newTask.appendChild(closeIcon);
+
+  newTask.classList.add("task");
+
+  taskContainer.appendChild(newTask);
+
+  closeIcon.addEventListener("click", () => {
+    taskContainer.removeChild(closeIcon.parentElement);
+    if (!taskContainer.querySelector("#addTask")) {
+      taskContainer.appendChild(addTask);
+    }
+  });
+
+  return { newTask, input };
 }
 
 export { buildInboxSection };
